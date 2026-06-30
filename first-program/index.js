3bebc1b13c1fb33428aa65bc35e0b5b1e289e417
SEMohebur <mdmohebur11@gmail.com>
2026-06-30 20:55:26 +0600

    first commit

---
+++ first-program/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let userName = "Mohibur Rahman";
+let num = 33;
+console.log(userName, num);
+// prothome ts file ke complie korte hobe //npx tsc index.ts
+// tarpor oi compiler ke run korte hobe //node index.js   diye karon tokhon seta js file converted
